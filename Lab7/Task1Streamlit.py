7d0f6695430e94b516c1813cffdf1192b09781c0
GABRIEL NELLIS <5089401@nwpolytech.ca>
2025-11-17 21:36:38 -0700

    test to see if backtrack wants to work with streamlit

--- Lab7/Task1Streamlit.py
+++ Lab7/Task1Streamlit.py
@@ -107,7 +107,7 @@ if run_full:
     placeholder.empty()
     result = backtracking_search(CSP)
     with placeholder:
-        buildGraph(CSP, result)
+        buildGraph(result)
 
 elif reset_clicked:
     placeholder.empty()
